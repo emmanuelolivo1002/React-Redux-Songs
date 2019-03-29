9b5286015177cdd422f25b070c37c4a4e3a5f718
Emmanuel Olivo <emmanuelo1002@gmail.com>
2019-03-29 10:45:43 -0500

    Create SongList Component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from "react";
+import SongList from "./components/SongList";
 
 const App = () => {
   return (
-    <div className="ui container">
-      <h1>Songs</h1>
+    <div className="ui container grid">
+      <div className="ui row">
+        <div className="column eight wide">
+          <SongList />
+        </div>
+      </div>
     </div>
   );
 };
